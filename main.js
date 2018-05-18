e4fb7764cec05d4beb499e7a63b35e28923b07e3
bodinj2018 <jbodin@sjnma.org>
2018-05-18 09:01:27 -0500

    asdf

---
+++ main.js
@@ -0,0 +1,30 @@
+alert("Welcome you are about to pertisipate in a madlib");
+
+
+
+
+var Name = prompt("Please enter your name", "");
+
+var Adjective1 = prompt("Please enter a Adjective", "");
+var Adjective2 = prompt("Please enter a Adjective", "");
+var Noun1 = prompt("Please enter a Noun", "");
+var Noun2 = prompt("Please enter a Noun", "");
+var PluralNoun1 = prompt("Please enter a Plural Noun", "");
+var Game1 = prompt("Please enter a Game", "");
+var PluralNoun2 = prompt("Please enter a Plural Noun", "");
+var Verbing1 = prompt("Please enter a Verb ending in ing", "");
+var Verbing2 = prompt("Please enter an Verb ending in ing", "");
+var PluralNoun3 = prompt("Please enter an Plural Noun", "");
+var Verbing3 = prompt("Please enter an Verb ending in ing", "");
+var Noun3 = prompt("Please enter an Noun", "");
+var Plant1 = prompt("Please enter a Plant", "");
+var BodyPart1 = prompt("Please enter a Body Part", "");
+var Place1 = prompt("Please enter a Place", "");
+var Verbing4 = prompt("Please enter a Verb ending in ing", "");
+var Adjective3 = prompt("Please enter a Adjective", "");
+var Number1 = prompt("Please enter a Number", "");
+var PluralNoun4 = prompt("Please enter a Plural Noun", "");
+
+
+document.getElementById("name").innerHTML = "A vacation is when you take a trip to some " + Adjective1 + " place with your " + Adjective2 + " family. Usually you go to some place that is near a/an " + Noun1 + " or up on a/an "+ Noun2 + ". A good vacation place is one where you can ride "+ PluralNoun1 + " or play " + Game1 +" or go hunting for "+ PluralNoun2 +". I like to spend my time " + Verbing1 + " or " + Verbing2  +". When parents go on a vacation, they spend their time eating three "+ PluralNoun3 +" a day, and fathers play golf, and mothers sit around " + Verbing3 + ". Last summer, my little brother fell  in  a/an "+ Noun3 +" and got poison "+ Plant1 +" all over his " + BodyPart1 + ". My family is going to go to (the) "+ Place1 + ", and I will practice "+ Verbing4 + ". Parents need vacations more than kids because parents are always very " + Adjective3 + " and because they have to work "+ Number1 + " hours every day all year making enough "+ PluralNoun4 +" to pay for the vacation. I hope that you had a laugh at this one " + Name +". ";
+//"A vacation is when you take a trip to some " + Adjective1 + "lace with your"
